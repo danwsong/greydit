7e081955818532be57e96823577f17015cda36a4
Daniel Song <danwsong@gmail.com>
2019-04-19 09:12:18 -0400

    log reddit + url

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http')
 var server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"})
     response.end("Hello World\n")
-    console.log(request.url)
+    console.log("https://reddit.com" + request.url + ".rss")
 })
 
 server.listen(process.env.PORT)
