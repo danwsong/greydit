4a0b57bafb3fcbd80da279ceeb57f91ec18a25bb
Daniel Song <danwsong@gmail.com>
2019-04-19 09:30:10 -0400

    add reddit url with link

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var http = require('http')
 
 var server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"})
-    response.end("Hello World\n")
-    console.log("https://reddit.com" + request.url + ".rss")
+    response.writeHead(200, {"Content-Type": "text/html"})
+    response.end("<html><body><a href=\"https://reddit.com" + request.url + "\">https://reddit.com" + request.url + "</a></body></html>")
 })
 
 server.listen(process.env.PORT)
