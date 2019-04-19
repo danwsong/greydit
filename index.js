af8bc15e152dc2ce9fb8c18c9cc113128240da87
Daniel Song <danwsong@gmail.com>
2019-04-19 13:57:23 -0400

    fix crash on visiting pages with no feed

--- index.js
+++ index.js
@@ -12,34 +12,31 @@ var server = http.createServer((req, res) => {
         })
         response.on('end', () => {
             xml2js.parseString(xml, (err, result) => {
-                console.dir(result)
-                if (result['feed']) {
-                    res.writeHead(200, {'content-type': 'text/html'})
-                    var html = '';
-                    html += '<html>';
-                    html += '<body>';
-                    if ('feed' in result) {
-                        var feed = result['feed'];
-                        if ('title' in feed) {
-                            html += '<h1>' + feed['title'][0] + '</h1>'
-                        }
-                        if ('subtitle' in feed) {
-                            html += '<h3>' + feed['subtitle'][0] + '</h3>'
-                        }
-                        if ('entry' in feed) {
-                            html += '<div id=\'feed\'>'
-                            feed['entry'].forEach((entry) => {
-                                html += '<p><a href=\'' + entry['link'][0]['$']['href'] + '\'>' + entry['title'] + '</a></p>'
-                            });
-                            html += '</div>'
-                        }
-                    } else {
-                        html += '<p>An error occurred.</p>'
+                res.writeHead(200, {'content-type': 'text/html'})
+                var html = '';
+                html += '<html>';
+                html += '<body>';
+                if (result && 'feed' in result) {
+                    var feed = result['feed'];
+                    if ('title' in feed) {
+                        html += '<h1>' + feed['title'][0] + '</h1>'
                     }
-                    html += '</body>';
-                    html += '</html>';
-                    res.end(html)
+                    if ('subtitle' in feed) {
+                        html += '<h3>' + feed['subtitle'][0] + '</h3>'
+                    }
+                    if ('entry' in feed) {
+                        html += '<div id=\'feed\'>'
+                        feed['entry'].forEach((entry) => {
+                            html += '<p><a href=\'' + entry['link'][0]['$']['href'] + '\'>' + entry['title'] + '</a></p>'
+                        });
+                        html += '</div>'
+                    }
+                } else {
+                    html += '<p>This part of Reddit is not supported yet.</p>'
                 }
+                html += '</body>';
+                html += '</html>';
+                res.end(html)
             })
         })
     }).on('error', (err) => {
