94b3ec1074af753556d01ac2980f3e3719532d33
Daniel Song <danwsong@gmail.com>
2019-04-18 23:05:39 -0400

    fixed crash?

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http')
 
 var server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"})
-    response.end(request)
+    response.end("Hello World\n")
 })
 
 server.listen(process.env.PORT)
