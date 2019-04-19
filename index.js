d3f0ec6eb9ab0d8efdb4425443be81ddd2d495f2
Daniel Song <danwsong@gmail.com>
2019-04-18 23:47:50 -0400

    add log request

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http')
 var server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"})
     response.end("Hello World\n")
+    console.log(request)
 })
 
 server.listen(process.env.PORT)
