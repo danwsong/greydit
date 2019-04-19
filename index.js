31552da1f646576574531d260104e35fcf86031e
Daniel Song <danwsong@gmail.com>
2019-04-18 23:02:04 -0400

    output request

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http')
 
-http.createServer(function(request, response) {
+var server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"})
-    response.end("Hello World\n")
-}).listen(process.env.PORT)
+    response.end(request)
+})
+
+server.listen(process.env.PORT)
